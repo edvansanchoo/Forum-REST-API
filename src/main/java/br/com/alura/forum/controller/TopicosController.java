83dda967faa1bd2732a7c402b6c1d604d0bb612c
edvansancho <edvansanchoo@gmail.com>
2021-07-29 17:13:38 -0400

    Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,25 +2,29 @@ package br.com.alura.forum.controller;
 
 
 import br.com.alura.forum.controller.Dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso){
 
         if(nomeCurso == null){
@@ -33,4 +37,14 @@ public class TopicosController {
             return TopicoDto.converter(topicos);
         }
     }
+
+    @PostMapping
+    public  void cadastrar(@RequestBody TopicoForm form){
+
+        Topico topico = form.converter(cursoRepository);
+
+        topicoRepository.save(topico);
+
+    }
 }
+
--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -22,6 +22,14 @@ public class Topico {
 	@ManyToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 
+	public Topico() {
+	}
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
 
 	@Override
 	public int hashCode() {
