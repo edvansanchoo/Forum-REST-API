3f17117e485380684a4543ce042e1367b5c3fb5e
edvansancho <edvansanchoo@gmail.com>
2021-10-07 15:05:36 -0400

    Autenticando o usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$3QKI.aVVu2w7/.tatMNzfOMwRxHKp2FNrNMQ7RpiD8cwDVu46ALci');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
