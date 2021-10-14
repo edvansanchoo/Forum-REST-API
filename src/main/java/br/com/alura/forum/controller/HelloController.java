83e3d0a8b40b1a752fdbee5eb1984f0fa90738a6
edvansancho <edvansanchoo@gmail.com>
2021-10-13 21:01:55 -0400

    Configurando autenticação Stateless

--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class Hello {
+public class HelloController {
 
     @RequestMapping("/")
     @ResponseBody
