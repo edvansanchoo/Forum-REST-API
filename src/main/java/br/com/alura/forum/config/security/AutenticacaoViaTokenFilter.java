ca5eb4a8beb9bb96520e564ffd01e13fd0b5a065
edvansancho <edvansanchoo@gmail.com>
2021-10-18 19:34:36 -0400

    Recuperando o token do header Authorization

---
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(httpServletRequest);
+        System.out.println(token);
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String recuperarToken(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization");
+        if(token == null || token.isEmpty() || ! token.startsWith("Bearer ")){
+            return null;
+        }
+        return token.substring(7, token.length());
+    }
+}
